4fb9a7c8537f36ac9efa43560966566fea775ecf
joe <joelpartey123@gmail.com>
2025-11-24 14:57:10 -0800

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,4 @@
 def perform_operation(num1,num2,operator):
-    num1 = float(input('enter the first number:'))
-    num2 = float(input('enter the second number:'))
-    operator = input('Enter the operation (Enter the operation (add, subtract, multiply, divide):').strip().lower()
     if(operator=='add'):
         return num1+num2
     elif(operator=='subtract'):
