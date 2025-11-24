307db388f32d80930bfbaf6f0879e4e0f52d5920
joe <joelpartey123@gmail.com>
2025-11-24 15:00:17 -0800

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,19 @@
-def perform_operation(num1,num2,operator):
-    if(operator=='add'):
-        return num1+num2
-    elif(operator=='subtract'):
-        return num1-num2
-    elif(operator=='multiply'):
-        return  num1*num2
-    elif(operator=='divide'):
-        if(num2==0):
-            return'cannot divide by 0'
-        else:
-            return num1/num2
+def perform_operation(num1, num2, operator):
+    operator = operator.strip().lower()
 
-    
+    if operator == "add":
+        return num1 + num2
+
+    elif operator == "subtract":
+        return num1 - num2
+
+    elif operator == "multiply":
+        return num1 * num2
+
+    elif operator == "divide":
+        if num2 == 0:
+            return "cannot divide by 0"
+        return num1 / num2
+
+    else:
+        return "invalid operation"
